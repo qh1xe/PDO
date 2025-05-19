c451a7fe00a6fc0580bde1f38207b9a6bdec6ebe
Chloe <chloewaldron2008@gmail.com>
2025-05-19 12:21:46 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -5,5 +5,16 @@ if (empty($_SESSION['login_status'])) {
 }
 
 echo "welkom!";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <a href="logout.php"> Logout </a>
+</body>
+</html>
